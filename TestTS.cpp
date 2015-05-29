247733e55ead95db7a0ae988754fbb108532a430
Eric Tang <eric.tang@tyo.com.au>
2015-05-29 14:51:57 +1000

    small updates

--- TestTS.cpp
+++ TestTS.cpp
@@ -9,7 +9,6 @@
  * TEST TEMPlATE SPECIFICATION
  */
 
-
 #include <iostream>
 #include <type_traits>
 
@@ -17,10 +16,40 @@
 #include <stddef.h>
 #include <stdint.h>
 
-class A {};
+using namespace std;
+
+template<typename T>
+class A {
+public:
+	T a_;
+	T result_;
+
+	A(T a) : a_(a) {}
+
+	void plus(T b) {
+		result_ = a_ + b;
+	}
+
+	void print() {
+		cout << result_ << endl;
+	}
+};
+
+template<>
+class A<double> :
+{
+	void plus(int b) {
+
+	}
+};
 
 int main()
 {
+	A<int> a(2);
+	a.plus(3);
+	a.print();
+
+	A<double> d(3.0);
 	return 0;
 }
 
